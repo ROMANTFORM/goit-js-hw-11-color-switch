f4191fced08dd1da9effae83a16ee283680f1428
Roman Tihonovskiy <romantform@gmail.com>
2021-04-04 13:38:33 +0300

    commit1

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    bodyEl: document.querySelector('body'),
+    btnStart: document.querySelector('.start'),
+    btnStop: document.querySelector('.stop'),
+};
+
+const startColors = {
+  intId: null,
+  start() {
+    this.intId = setInterval(() => {
+      colors.map((color, ind) => {
+        if (ind === randomIntegerFromInterval(0, ind)) {
+          refs.bodyEl.style.backgroundColor = color;
+        }
+      });
+      console.log('iiii')
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intId);
+  },
+};
+
+refs.btnStart.addEventListener('click', () => {
+  startColors.start();
+});
+
+refs.btnStop.addEventListener('click', () => {
+  startColors.stop();
+});
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
